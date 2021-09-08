1c41da14dcca070c4ced892f2fdba1d6580192d2
pradumnasaraf <pradumnasaraf@gmail.com>
2021-09-08 10:08:01 +0530

    Changes in sleep the time and also added comments

--- F1Bot.py
+++ F1Bot.py
@@ -1,25 +1,39 @@
 import tweepy
 import time
-
-from tweepy import cursor
                
 auth = tweepy.OAuthHandler('95IVnwKaS1kH99tUX5ms1Loe4','vFkwVcZ5fHGmBpXfoRk5iVNrNzFh5RwVu0mL717cKhNpRB6d6b')
 auth.set_access_token('1434857266247729158-tXWHFHE52W5mR24KZt9lo3xrCcdhK6','dHaZx4Ebja0xqgvBnpIMravspdIVaIet7f8cXOLdv2jyN')
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-user = api.me()
+search_term = '#f1'
+TweetNumber = 250
 
-search = '#f1'
-search1 = '#formula1'
-noTweet = 500
+tweets = tweepy.Cursor(api.search,search_term).items()# Argumnets =TweetNumber
 
-for tweet in tweepy.Cursor(api.search, search).items(noTweet):
+#Counter to keep track of no of tweets
+count =0
+
+# Taking out a tweets from the Array of tweet.
+for tweet in tweets:
+    count = count +1
+
+    #After tweeting for 50 tweets Bot will sleep for 60 Seconds.
+    if count ==50:
+        count =0
+        time.sleep(250)
+
+    #Try to like the tweet
     try:
-        print('Tweet Liked')
+        print('Tweet Liked NO- '+ str(count))
+    
+        #favorite() method is liking the tweet
         tweet.favorite()
-        time.sleep(10)
+
+        # After every like bot is sleeping for 5 Seconds.
+        time.sleep(5)
+
+    # Handeling any kind of exception (eg; tweet already liked some limitation error) 
     except tweepy.TweepError as e:
         print(e.reason)
-    except StopAsyncIteration:
-        break
+        time.sleep(5)
