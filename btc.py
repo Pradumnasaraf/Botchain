41049957ef7d667af24495813b0730d12d39bb91
pradumnasaraf <pradumnasaraf@gmail.com>
2021-11-04 23:22:42 +0530

    Changes in the API company

--- btc.py
+++ btc.py
@@ -1,7 +1,9 @@
+from requests.sessions import Session
 import tweepy
 import time
 import json
 import requests
+import typing
 
 consumer_key = ''
 consumer_secret = ''
@@ -13,36 +15,36 @@ auth.set_access_token(key,secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 Ticker =1
+
+url ='https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+parameters={
+    'slug':'bitcoin',
+}
+
+headers ={
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY':''
+}
+session = Session()
+session.headers.update(headers)
+
 while True:
 
     try:
-        # Using get api method getting lastest price of bitcoin in USD
-        BitCoin = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-        
-        # callling json() on the string.
-        price = BitCoin.json()
-
-        # Getting the value by using keys
-        Coin = price['bitcoin']['usd']
-
-        # Meassge that will include in the tweet
-        FinalMessage = 'Current #Bitcoin Price is '+ str(Coin)+'$ #BTC #Crypto'
-
-        # update_status let us to post tweet having some kind of content
+        response = session.get(url,params=parameters)
+        Data = json.loads(response.text)['data']['1']['quote']['USD']['price']
+        FinalMessage = 'Current #Bitcoin Price is $'+ str(round(Data))+' #BTC #Crypto'
         api.update_status(FinalMessage)
 
-        #Added Ticket to keep track of no of tweet bot has tweeted.
         print('Tweet No.'+str(Ticker))
         Ticker= Ticker+1
 
-        # Bot sleeping 5 min after Every tweet
         time.sleep(300)
 
-    # Hadling Any kind of errors like (Responce error, Simliar tweet)
+
     except tweepy.TweepError as e:
         print(e.reason)
-
-        #Printing the error message
         print('Tweet No.'+str(Ticker))
         Ticker= Ticker+1
         time.sleep(300)
--- doge.py
+++ doge.py
@@ -1,7 +1,9 @@
+from requests.sessions import Session
 import tweepy
 import time
 import json
 import requests
+import typing
 
 consumer_key = ''
 consumer_secret = ''
@@ -13,36 +15,37 @@ auth.set_access_token(key,secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 Ticker =1
+
+url ='https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+parameters={
+    'slug':'dogecoin',
+}
+
+headers ={
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY':''
+}
+session = Session()
+session.headers.update(headers)
+
 while True:
 
     try:
-        # Using get api method getting lastest price of doge in USD
-        DogeCoin = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd')
-
-        # callling json() on the string.
-        price = DogeCoin.json()
-
-        # Getting the value by using keys
-        Coin = price['dogecoin']['usd']
-
-        # Meassge that will include in the tweet
-        FinalMessage = 'Current #DOGE Price is '+ str(Coin)+'$ #Dogecoin #Crypto #ToTheMoon'
-
-        # update_status let us to post tweet having some kind of content
+        response = session.get(url,params=parameters)
+        Data = json.loads(response.text)['data']['74']['quote']['USD']['price']
+        formatted = '{0:.6g}'.format(Data)
+        FinalMessage = 'Current #DOGE Price is $'+str(formatted)+' #Dogecoin #Crypto #ToTheMoon🌕'
         api.update_status(FinalMessage)
 
-        #Added Ticket to keep track of no of tweet bot has tweeted.
         print('Tweet No.'+str(Ticker))
         Ticker= Ticker+1
 
-        ##Bot sleeping for 5 min, after Every tweet
         time.sleep(300)
 
-    # Hadling Any kind of errors like (Responce error, Simliar tweet)
+
     except tweepy.TweepError as e:
         print(e.reason)
-
-        #Printing the error message
         print('Tweet No.'+str(Ticker))
         Ticker= Ticker+1
         time.sleep(300)
